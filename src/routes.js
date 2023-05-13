e3c03c7261981473e74228b02a1600ba4f890182
Tornike Khatiashvili <toko36106@gmail.com>
2023-05-13 14:37:26 +0400

    add router

---
+++ src/routes.js
@@ -0,0 +1,19 @@
+import React from "react"
+import {Switch,Route,BrowserRouter} from "react-router-dom"
+import Header from "./Components/Header_footer/Header"
+import Footer from "./Components/Header_footer/Footer"
+import Home from "./Components/Home"
+ 
+function Routes() {
+  return (
+    <BrowserRouter>
+        <Header/> 
+        <Switch>
+          <Route path="/" exact component={Home}/>
+        </Switch>
+        <Footer/>
+    </BrowserRouter>
+  );
+}
+
+export default Routes;
